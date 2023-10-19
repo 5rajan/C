a9867875e562019e628262eff2db781ffc3437d1
Srajan Rai <02srajan@gmail.com>
2023-10-19 21:51:08 +0530

    Update 1-demo.c

--- 1-demo.c
+++ 1-demo.c
@@ -2,6 +2,11 @@
 
 int main() {
     system("cls");
-    printf("Hello World\n");
+    // printf("Hello World\n");
+
+    int i = 4;
+    for(;i;) printf("%d ", i--);
+    // when looping, look for 3 conditons used in for
+    
     return 0;
 }
